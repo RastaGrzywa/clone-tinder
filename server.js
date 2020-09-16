d5b015f209c862c91b60fbc3f6131190d3e5ee2f
RastaGrzywa <andrzej@fejklowicz.com>
2020-09-16 09:45:17 +0200

    server add build mode

--- server.js
+++ server.js
@@ -58,6 +58,10 @@ if(process.env.NODE_ENV === 'production') {
       res.sendfile(path.join(__dirname = 'client/build/index.html'));
   })
 };
+// //build mode
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/public/index.html'));
+});
 
 // listener
 app.listen(port, () => {
